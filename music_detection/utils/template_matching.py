Fixed matching by adding normalization

music_detection/utils/template_matching.py
@@ -16,21 +16,17 @@ def match(template: np.array, image: np.array) -> Tuple[float, Tuple]:
     """
     found = None
 
-    for scale in np.linspace(0.5, 1.5, 50):
+    for scale in np.linspace(0.5, 1.5, 20):
         resized_template = cv2.resize(template, None, fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
         if resized_template.shape[0] > image.shape[0] or resized_template.shape[1] > image.shape[1]:
             break
 
-        result = cv2.matchTemplate(image, resized_template, cv2.TM_CCOEFF)
+        result = cv2.matchTemplate(image, resized_template, cv2.TM_CCOEFF_NORMED)
         (_, confidence, _, max_position) = cv2.minMaxLoc(result)
         if found is None or confidence > found[0]:
             found = (confidence, max_position, scale)
 
     (score, position, scale) = found
-    print(score)
-    cv2.rectangle(image, position, (position[0]+int(template.shape[1]*scale), position[1]+int(template.shape[0]*scale)), 0)
-    cv2.imshow("", image)
-    cv2.waitKey()
     return score, position
 
 
@@ -45,9 +41,3 @@ def pick_template(template_list: List[Template], image: np.array) -> Enum:
             max_score = score
 
     return enum_type
-
-
-if __name__ == '__main__':
-    image = cv2.imread("../../resources/test_images/twinkle.jpg")
-    template = cv2.imread("../../resources/templates/time/68.jpg")
-    match(template, image)