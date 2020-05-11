311e7909c3fdc3c0e68a9ca5f45d3a7e33665adf
Jerome <j.m.b.r.sauger@student.utwente.nl>
2020-05-11 11:24:53 +0200

    added an image attribute

--- music_detection/measure.py
+++ music_detection/measure.py
@@ -1,8 +1,10 @@
+
 class Measure:
-    def __init__(self):
+    def __init__(self, image=None):
         self.position_in_image = []
         self.note_list = []
         self.time_signature = 0
+        self.image = image #Actual picture of the staff
 
     def identify_notes(self):
         pass
