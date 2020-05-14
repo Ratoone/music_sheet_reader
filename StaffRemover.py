babd871cb0cb2322e258a5733cfe397c3e6ac494
rshahmoradii <60846735+rshahmoradii@users.noreply.github.com>
2020-05-14 10:56:45 +0200

    making functions for preproccesing, staff removal,staff detection

--- StaffRemover.py
+++ StaffRemover.py
@@ -1,13 +1,16 @@
 # Python program to read image using OpenCV
 # importing OpenCV(cv2) module
+import PIL
 import cv2
 import numpy as np
+from PIL import Image
+
 
 # Read the image
 img = cv2.imread('twinkle.jpeg')
 
 
-def staffDetection(img):
+def PreProccesing(img):
     # remove noise
     kernel = np.ones((3, 3), np.float32) / 9
     dst = cv2.filter2D(img, -1, kernel)
@@ -18,12 +21,15 @@ def staffDetection(img):
     # ret2,th2 = cv2.threshold(gray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     # canny_edge detection
     edges = cv2.Canny(gray, 50, 150, apertureSize=3)
-    cv2.imwrite('edges.jpg', edges)
     # find the lines using Hough Transform
     lines = cv2.HoughLines(edges, 1, np.pi / 180, 450)
     # sort the lines based on their distance from the origin (top left)
     linesSorted = np.sort(lines, axis=0)
+    return linesSorted, edges
 
+
+def staffDetection(img):
+    linesSorted, edges = PreProccesing(img)
     # find number of all the staff lines
     numberOfLines = int(linesSorted.shape[0] / 2)
     # find number of staves (in twinkle case it is 3)
@@ -41,38 +47,43 @@ def staffDetection(img):
         s = int((numberOfLines * 2 / numberOfStave) * (i + 1))
         distance[i + 1][0] = linesSorted[s - 1, 0][0] + round((linesSorted[s, 0][0] - linesSorted[s - 1, 0][0]) / 2)
         distance[i + 1][1] = linesSorted[s - 1, 0][1]
-        return distance, lineGap
+    return distance
 
 
-distance, lineGap = staffDetection(img)
-for rho, theta in distance[:]:
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a * rho
-    y0 = b * rho
-    x1 = int(x0 + 2000 * (-b))
-    y1 = int(y0 + 2000 * (a))
-    x2 = int(x0 - 2000 * (-b))
-    y2 = int(y0 - 2000 * (a))
-    #cv2.imshow('img',img)
-    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 1)
-    #cv2.waitKey(0)
-cv2.imwrite('reytest.jpg', img)
+distance = staffDetection(img)
 
 
-# staffRemoval
-for i in range(linesSorted.shape[0]):
-    x = int(linesSorted[i, 0][0])
-    edges[x, :] = 0
-# do closing operation
-kernel = np.ones((5,5),np.uint8)
-closing = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
-# complement of the image
-imageComp = cv2.bitwise_not(closing)
-cv2.imwrite('houghlines5.jpg', imageComp)
+def CropImage(distance):
+    numberOfStave = int(distance.shape[0])
+    crop_img = []
+    for i in range(numberOfStave - 1):
+        y = int(distance[i][0])
+        h = int(distance[i + 1][0] - distance[i][0])
+        crop_img.append(img[y:y + h, :, :])
+    return crop_img
 
-# this is to test how the lines are found
+
+linesSorted, edges = PreProccesing(img)
+
+
+def StaffRemoval(img):
+    # staffRemoval
+    for i in range(linesSorted.shape[0]):
+        x = int(linesSorted[i, 0][0])
+        edges[x, :] = 0
+    # do closing operation
+    kernel = np.ones((5, 5), np.uint8)
+    closing = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+    # complement of the image
+    imageComp = cv2.bitwise_not(closing)
+    return imageComp
+
+
+imageComp = StaffRemoval(img)
+cv2.imshow('r', imageComp)
+cv2.waitKey(0)
 '''
+# this is to test how the lines are found
 for rho, theta in linesSorted[:, 0]:
     a = np.cos(theta)
     b = np.sin(theta)
