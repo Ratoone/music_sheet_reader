13d3505dfc09054692e0d2778dc6492e32bf3498
Jerome <j.m.b.r.sauger@student.utwente.nl>
2020-05-25 15:29:21 +0200

    changed Note constructor so that the attributes can be directly set

--- music_detection/note.py
+++ music_detection/note.py
@@ -2,8 +2,13 @@ from .note_enum import NoteEnum
 
 
 class Note:
-    def __init__(self):
-        self.name = NoteEnum.UNDEFINED
-        self.octave = 0
-        self.duration = 0
+    def __init__(self, *args):
+        if (len(args)> 0) :
+            self.name = args[0]
+            self.octave= args[1]
+            self.duration = args[2]
+        else :
+            self.name = NoteEnum.UNDEFINED
+            self.octave = 0
+            self.duration = 0
 
