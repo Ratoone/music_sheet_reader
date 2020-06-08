81e95ba9d86ccb25b290248f780b5a362c0b7fb2
Jerome <j.m.b.r.sauger@student.utwente.nl>
2020-06-08 19:30:30 +0200

    changed PreprocessingManager to a simpler version

--- music_detection/utils/preprocessing_manager.py
+++ music_detection/utils/preprocessing_manager.py
@@ -1,108 +1,48 @@
 import cv2
 import numpy as np
 
-from preprocessing_operations_enum import PreprocessingOperationEnum as PEnum
-
 class PreprocessingManager :
-    """Manages a whole preprocessing workflow, all operations are performed in-place,\n
-    can be retrieved any time by accessing the processed_img attribute
-     :img_to_be_processed: numpy ndarray, initial image, format does not matter
-     :operations_list: list of preprocessing operations to perform, this list has to like that :
-     [operation1:PEnum, [operation2:PEnum, arg1, arg2, ...], operation3:PEnum,...]. Refer to the __perform_operation method
-     for more detailed explanations
-    """
-    def __init__(self,img_to_be_processed:np.ndarray, operations_list=[]):
-        self.operations_list = operations_list
-        self.processed_img= img_to_be_processed.copy()
+    def __init__(self, img:np.ndarray=None):
+        self.image=img 
 
-    def __repr__(self) :
-        repr_string = "operations queued :\n"
-        for element in self.operations_list :
-            repr_string += str(self.__read_operation(element).name)+"\n"
-        return repr_string
-
-    def add_operation(self, operation) -> None:
-        """Appens an operation to PreprocessingManager.operations_list\n
-        :operation: PEnum or a list of the following form : [operation:PEnum, arg1, arg2...]"""
-        self.operations_list.append(operation)
-    
-    def remove_operation(self, index=-1) -> None:
-        """Removes a preprocessing operation from the operations list \n
-        :index: int, index of the operation in PreprocessingManager.operations_list
-        to remove
+    def generate_edge_map(self, binary_inv:bool=True) -> np.ndarray :
         """
-       self.operations_list.pop(index)
-    
-    def launch_preprocessing(self) -> None:
-        """Launches the preprocessing. Operations are removed one-by-one\n
-        from PreprocessingManager.operations_list and executed
+        Perform an edge detection using Canny algorithm on PreprocessingManager.image \n
+         :param binary_inv: bool, if true the edges are in white on the resulting edge map. They are black otherwise.
+         :return : np.ndarray, binary image containing the edges found  
         """
-        for i in range(len(self.operations_list)) :
-            element = self.operations_list.pop(i)
-            self.__perform_operation(element)
+        kernel = np.ones((3, 3), np.float32) / 9
+        dst = cv2.filter2D(self.image, -1, kernel)
+        # canny_edge detection
+        edges = cv2.Canny(dst, 50, 150, apertureSize=3)
 
-    def __read_operation(self, element) :
-        """Extracts the type of operation from an element of PreprocessingManager.operations_list
-        :element: PEnum or a list of the following form : [operation:PEnum, arg1, arg2...]
+        if binary_inv ==False :
+            edges = 255-edges
+        
+        return edges
+
+    def generate_thresholded_image(self, binary_inv:bool=True) -> np.ndarray :
         """
-        if type(element)==list :
-            operation = element[0]
-        else :
-            operation = element
-        return operation
-
-    def __perform_operation(self, element) :
-        """Performs in-place the operation stored in element\n
-        :element: PEnum or a list of the following form : [operation:PEnum, arg1, arg2...]
-
-        The following operations can be performed :
-            -convert image to grayscale
-                Expected element : PEnum.CONVERT2GRAY
-            -blurr image
-                Expected element : [PEnum.BLURR, kernel_size:Tuple[int, int]]
-            -adaptive thresholding :
-                Expected element : [PEnum.ADAPTIVE_THRESHOLD, adaptive_method:cv2 constant, block_size:int]
-                    See cv2.adaptiveThreshold function for more information
-            -edge detection :
-                Expected element : [PEnum.EDGE_DETECTION, threshold1:int, threshold2:int, apertureSize:int]
-                    See cv2.Canny function for more information
-            -morphological opening :
-                Expected element : [PEnum.OPEN, kernel_size:Tuple[int, int]]
-            -morphological closing :
-                Expected element : [PEnum.CLOSE, kernel_size:Tuple[int, int]]
-            -skeletonization : 
-                Expected element  : PEnum.SKELETONIZE
+        Thresholds PreprocessingManager.image \n
+         :param binary_inv: bool, if true the black regions of the original image are in white on the resulting edge map. Otherwise they remain black
+         :return: np.ndarray, thresholded image
         """
-        operation = self.__read_operation(element)
-        if operation == PEnum.CONVERT2GRAY :
-            self.processed_img = cv2.cvtColor(self.processed_img, cv2.COLOR_RGB2GRAY)
+        im_gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        im_blur = cv2.GaussianBlur(im_gray, (3, 3), 0)
+        im_adapt_thresh = cv2.adaptiveThreshold(im_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+                            cv2.THRESH_BINARY_INV, 11, 2)
+        #Closing operation
+        #kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+        #im_morph = cv2.morphologyEx(im_adapt_thresh, cv2.MORPH_CLOSE, kernel)
 
-        elif operation == PEnum.BLURR :
-            kernel = element[1]
-            self.processed_img = cv2.GaussianBlur(self.processed_img, kernel)
-        elif operation == PEnum.ADAPTIVE_THRESHOLD :
-            self.processed_img = cv2.adaptiveThreshold(self.processed_img, 255,element[1], element[2],
-                                                        element[3],2) 
+        im_morph=im_adapt_thresh.copy()
+        
+        if binary_inv ==False :
+            im_morph=255-im_morph
+        
+        return im_morph
 
-        elif operation == PEnum.EDGE_DETECTION :
-            self.processed_img = cv2.Canny(self.processed_img, element[1], element[2], apertureSize=element[3])
 
-        elif operation == PEnum.OPEN :
-            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, element[1])
-            self.processed_img = cv2.morphologyEx(self.processed_img, cv2.MORPH_OPEN, kernel)
 
-        elif operation==PEnum.CLOSE :
-            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, element[1])
-            self.processed_img = cv2.morphologyEx(self.processed_img, cv2.MORPH_CLOSE, kernel)
 
-        elif operation == PEnum.SKELETONIZE :
-            im_skel = np.zeros(self.processed_img.shape, np.uint8)
-            element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-            while cv2.countNonZero(self.processed_img) != 0:
-                im_open = cv2.morphologyEx(self.processed_img, cv2.MORPH_OPEN, element)
-                temp = cv2.subtract(self.processed_img, im_open)
-                eroded = cv2.erode(self.processed_img, element)
-                im_skel = cv2.bitwise_or(im_skel, temp)
-                self.processed_img = eroded.copy()
-            self.processed_img = im_skel
         
