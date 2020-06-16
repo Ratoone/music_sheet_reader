da1006c82a416e807941a6d14acf59ffefb8bc2d
Jerome <j.m.b.r.sauger@student.utwente.nl>
2020-06-16 09:15:09 +0200

    Corrected test error

--- test/staff_splitting_test.py
+++ test/staff_splitting_test.py
@@ -8,5 +8,5 @@ from music_detection.utils.StaffRemover import staffDetection
 def test_count_staves_successfully():
     path = os.path.dirname(os.path.abspath(__file__))
     image = cv2.imread(os.path.join(path, "../resources/test_images/twinkle.jpg"))
-    staves, _ = staffDetection(image)
+    staves, _,_ = staffDetection(image)
     assert len(staves) == 3
