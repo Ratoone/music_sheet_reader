754ac031e6b572e83ca1e6fe4c7c07c1d65002b5
Ratoone <vacariu.paul@gmail.com>
2020-07-21 00:33:47 +0200

    Fixed template test

--- test/template_test.py
+++ test/template_test.py
@@ -5,6 +5,7 @@ import pytest
 
 from music_detection.key_enum import KeyEnum
 from music_detection.time_enum import TimeSignatureEnum
+from music_detection.utils.preprocessing_functions import generate_thresholded_image
 from music_detection.utils.template_manager import TemplateManager
 from music_detection.utils.template_matching import pick_template
 
@@ -20,9 +21,11 @@ def template_manager():
 
 def test_key(template_manager):
     image = cv2.imread(os.path.join(path, "../resources/test_images/twinkle.jpg"))
+    image = generate_thresholded_image(image, True)
     assert KeyEnum.SOL == pick_template(template_manager.clef, image)
 
 
 def test_time(template_manager):
     image = cv2.imread(os.path.join(path, "../resources/test_images/twinkle.jpg"))
+    image = generate_thresholded_image(image, True)
     assert TimeSignatureEnum.COMMON == pick_template(template_manager.time_signature, image)
