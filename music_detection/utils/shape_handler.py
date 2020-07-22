66cc821077a80585df8d3fb839d5f4c3b85982a4
Ratoone <vacariu.paul@gmail.com>
2020-07-22 23:34:44 +0200

    Added non-template filtering wrt vertical position

--- music_detection/utils/shape_handler.py
+++ music_detection/utils/shape_handler.py
@@ -13,6 +13,10 @@ from music_detection.utils.template_matching import pick_template
 path = os.path.dirname(os.path.abspath(__file__))
 template_manager = TemplateManager(os.path.join(path, "../../resources/templates"))
 
+# the default note will be the Si4 - third line, Sol clef - as it is in the center of the image
+DEFAULT_NOTE_PITCH = 7 * 4 + 6
+MAXIMUM_PITCH_DIFFERENCE = 4
+
 
 def get_adjusted_line_gap(line_gap: int) -> int:
     return int(line_gap * 0.8)
@@ -20,9 +24,11 @@ def get_adjusted_line_gap(line_gap: int) -> int:
 
 class ShapeHandler:
     @staticmethod
-    def identify_shape(image: np.ndarray, key: KeyEnum, line_gap: int) -> Tuple[str, object]:
+    def identify_shape(image: np.ndarray, upper_limit: int, element_height: int, key: KeyEnum, line_gap: int) -> Tuple[str, object]:
         """
         Identify the current shape using various methods: template matching, hough transform, etc.
+        :param upper_limit: the upper bound of the identified element
+        :param element_height: the height of the element in the image
         :param image: the element to be identified
         :param key: the key of the staff. Useful for identifying the right pitch of the note
         :param line_gap: the line gap in pixels between two staff lines. Useful for notes
@@ -37,8 +43,14 @@ class ShapeHandler:
         if time is not None:
             return "time", time
 
+        # filter out the non-template elements that are far from the staff
+        height, _ = image.shape
+        theoretical_pitch_distance = (height - 2 * upper_limit - element_height) / line_gap
+        if abs(theoretical_pitch_distance) > MAXIMUM_PITCH_DIFFERENCE:
+            return "invalid", None
+
         line_empty_gap = get_adjusted_line_gap(line_gap)
-        note_heads = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, 1.2, line_empty_gap, minRadius=int(0.8*line_empty_gap), maxRadius=int(1.4*line_empty_gap), param1=50, param2=5)
+        note_heads = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, 1.2, line_empty_gap, minRadius=int(0.7*line_empty_gap), maxRadius=line_empty_gap, param1=50, param2=5)
         if note_heads is not None:
             # TODO: assuming single note, fix for 2 eights
             return "note", ShapeHandler.handle_note(image, key, line_gap, int(note_heads[0][0][0]), int(note_heads[0][0][1]))
@@ -65,10 +77,10 @@ class ShapeHandler:
         else:
             # center is empty (i.e. average around center is black) => half note
             if np.average(image[
-                          note_center_v - line_gap//2:
-                          note_center_v + line_gap//2,
-                          note_center_h - line_gap//2:
-                          note_center_h + line_gap//2]) < 0.75 * 255:
+                          note_center_v - line_gap//4:
+                          note_center_v + line_gap//4,
+                          note_center_h - line_gap//4:
+                          note_center_h + line_gap//4]) < 0.75 * 255:
                 note_duration = 2
             else:
                 # width is no bigger than 2 line gaps (implying the existence of the little flags)
@@ -77,10 +89,8 @@ class ShapeHandler:
                 else:
                     note_duration = 0.5
 
-        # the default note will be the Si4 - third line, Sol clef - as it is in the center of the image
-        default_note_pitch = 7 * 4 + 6
         # count the increments of half line gap between the note and the third line
-        note_pitch = default_note_pitch + int((height - 2 * note_center_v) / line_gap)
+        note_pitch = DEFAULT_NOTE_PITCH + int(np.round((height - 2 * note_center_v) / line_gap))
         # adapt the note to the key
         if key == KeyEnum.FA:
             note_pitch -= 12
