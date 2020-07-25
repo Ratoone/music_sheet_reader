3019f3c30880bf9e359a96b1b4b6d8ecb2979d6b
Ratoone <vacariu.paul@gmail.com>
2020-07-25 21:28:03 +0200

    Performing note detection on reduced element image

--- music_detection/utils/shape_handler.py
+++ music_detection/utils/shape_handler.py
@@ -36,7 +36,6 @@ class ShapeHandler:
         :return: a string denoting the type of element found and an object relevant to that type.
         For instance, a note will return a Note, a clef will return an Enum, etc.
         """
-
         element_image = image[upper_limit:upper_limit + element_height, :]
 
         time = pick_template(template_manager.time_signature, element_image)
@@ -47,13 +46,13 @@ class ShapeHandler:
         if clef is not None:
             return "clef", clef
 
+        accidental = pick_template(template_manager.accidentals, element_image)
+        if accidental is not None:
+            return "accidental", accidental
+
         rest = pick_template(template_manager.rests, element_image)
         if rest is not None:
             return "rest", Note(NoteEnum.REST, 0, rest.value)
-
-        accidental = pick_template(template_manager.accidentals, element_image)
-        if accidental is not None:
-            pass
 
         # filter out the non-template elements that are far from the staff
         height, _ = image.shape
@@ -62,10 +61,10 @@ class ShapeHandler:
             return "invalid", None
 
         line_empty_gap = get_adjusted_line_gap(line_gap)
-        note_heads = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, 1.2, line_empty_gap, minRadius=int(0.7*line_empty_gap), maxRadius=line_empty_gap, param1=50, param2=5)
+        note_heads = cv2.HoughCircles(element_image, cv2.HOUGH_GRADIENT, 1.2, line_empty_gap, minRadius=int(0.7*line_empty_gap), maxRadius=line_empty_gap, param1=50, param2=5)
         if note_heads is not None:
             # TODO: assuming single note, fix for 2 eights
-            return "note", ShapeHandler.handle_note(image, key, line_gap, int(note_heads[0][0][0]), int(note_heads[0][0][1]))
+            return "note", ShapeHandler.handle_note(image, key, line_gap, int(note_heads[0][0][0]), int(note_heads[0][0][1]) + upper_limit)
 
         return "invalid", None
 
@@ -73,7 +72,8 @@ class ShapeHandler:
     def handle_note(image: np.ndarray, key: KeyEnum, line_gap: int, note_center_h: int, note_center_v: int) -> Note:
         """
         If the identified object is a note, find its pitch by checking the vertical position in the staff
-        :param note_center_position: the position of the center of the note
+        :param note_center_h: the horizontal position of the center of the note
+        :param note_center_v: the vertical position of the center of the note
         :param image: the object's cropped image from the music score
         :param key: the key of the staff - used to map the height to the pitch
         :param line_gap: the line gap, in pixels
@@ -83,7 +83,7 @@ class ShapeHandler:
         # TODO: consider the 1/16th note as well
         height, width = image.shape
         # no line => full note
-        lines = cv2.HoughLinesP(image, 1, np.pi / 180, int(line_gap * 2.5))
+        lines = cv2.HoughLinesP(image, 1, np.pi / 180, int(line_gap * 2.5), minLineLength=int(line_gap * 2.5))
         if lines is None:
             note_duration = 4
         else:
